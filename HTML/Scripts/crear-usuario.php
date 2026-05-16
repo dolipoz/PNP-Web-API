bbcc49120176b9c722344c29788a7d3cf52ad692
daniel <dolipoz@g.educaand.es>
2026-05-16 21:20:59 +0200

    HTML agregar usuarios

---
+++ HTML/Scripts/crear-usuario.php
@@ -0,0 +1,37 @@
+<?php
+	include "variables.php";
+	include "funciones.php";
+	include "conectar-db.php";
+
+	$usuario = $_POST['usuario'];
+	$clave = password_hash($_POST['clave'], PASSWORD_DEFAULT);
+	$correo = $_POST['correo'];
+	$nombre = $_POST['nombre'];
+	$apellidos = $_POST['apellidos'];
+	$activo = isset($_POST['activo']) ? 1 : 0;
+	$id_rol = $_POST['id_rol'];
+
+	$sql_signup="insert into usuarios (usuario,clave,correo,nombre,apellidos,activo,id_rol) values (
+		'$usuario',
+		'$clave',
+		'$correo',
+		'$nombre',
+		'$apellidos',
+		$activo,
+		$id_rol
+	)";
+
+    if ($conexion->query($sql_signup) == True) {
+        echo "Usuario añadido.";
+		$_SESSION["correcto"] = True;
+		$_SESSION["info"] = "Usuario se añadió correctamente.";
+    } else {
+		$_SESSION["error"] = True;
+		$_SESSION["info"] = "Usuario no se pudo añadir.";
+	}
+
+	header("Location: ../index.php");
+
+
+
+?>
--- HTML/index.php
+++ HTML/index.php
@@ -22,15 +22,15 @@
                 <label class='prompt'>PS C:\Haga click en Cerrar Sesión para salir></label><input type='submit' value='Cerrar Sesión'>
             </form>
         </div>
-        <form id='consola4' class='consolas' action='Scripts/agregar-usuarios.php' method='POST' style='display: none;'>
-            <label class='prompt' for='usuario'>PS C:\Escriba su usuario></label><input type='text' id='usuario' name='usuario' placeholder='|' size='20' min='8' max='20' required><br>
-            <label class='prompt' for='clave'>PS C:\Escriba su contraseña></label><input type='password' id='clave' name='clave' placeholder='|' size='20' min='8' max='20' required><br>
-            <label class='prompt' for='correo'>PS C:\Escriba su correo electrónico></label><input type='correo' id='correo' name='correo' placeholder='|' size='50' min='9' max='50'><br>
-            <label class='prompt' for='nombre'>PS C:\Escriba su nombre></label><input type='text' id='nombre' name='nombre' placeholder='|' size='20' min='2' max='20'><br>
-            <label class='prompt' for='apellidos'>PS C:\Escriba su[s] apellido[s]></label><input type='text' id='apellidos' name='apellidos' placeholder='|' size='50' min='2' max='50'><br>
+        <form id='consola4' class='consolas' action='Scripts/crear-usuario.php' method='POST' style='display: none;'>
+            <label class='prompt' for='usuario'>PS C:\Escriba su usuario></label><input type='text' id='usuario' name='usuario' placeholder='|' size='20' maxlength='20' required><br>
+            <label class='prompt' for='clave'>PS C:\Escriba su contraseña></label><input type='password' id='clave' name='clave' placeholder='|' size='20' minlength='8' maxlength='20' required><br>
+            <label class='prompt' for='correo'>PS C:\Escriba su correo electrónico></label><input type='email' id='correo' name='correo' placeholder='|' size='50' minlength='9' maxlength='50'><br>
+            <label class='prompt' for='nombre'>PS C:\Escriba su nombre></label><input type='text' id='nombre' name='nombre' placeholder='|' size='20' minlength='2' maxlength='20'><br>
+            <label class='prompt' for='apellidos'>PS C:\Escriba su[s] apellido[s]></label><input type='text' id='apellidos' name='apellidos' placeholder='|' size='50' minlength='2' maxlength='50'><br>
             <label class='prompt' for='activo'>PS C:\Marque si quiere activar el usuario></label><input type='checkbox' id='activo' name='activo' checked><br>
-            <label class='prompt' for='rol'>PS C:\Seleccione el rol del usuario></label>
-            <select id='rol' name='rol'>
+            <label class='prompt' for='id_rol'>PS C:\Seleccione el rol del usuario></label>
+            <select id='id_rol' name='id_rol'>
         ";
         $q_rol="select id,rol from roles";
         $roles=mysqli_query($conexion,$q_rol);
